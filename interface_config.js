578ddc767c70a635f876e9180f95e5a8400a06f0
Hai Pham <bienxanhhaiduong@gmail.com>
2021-12-29 08:55:35 +0700

    custom logo for sumato times 3

--- interface_config.js
+++ interface_config.js
@@ -27,8 +27,8 @@ var interfaceConfig = {
     CLOSE_PAGE_GUEST_HINT: false, // A html text to be shown to guests on the close page, false disables it
 
     DEFAULT_BACKGROUND: '#040404',
-    DEFAULT_LOGO_URL: 'images/sumato/Sumato-Online-Logo.png',
-    DEFAULT_WELCOME_PAGE_LOGO_URL: 'images/sumato/Sumato-Online-Logo.png',
+    DEFAULT_LOGO_URL: 'images/sumato/Sumato-Online-Logo-2.png',
+    DEFAULT_WELCOME_PAGE_LOGO_URL: 'images/sumato/Sumato-Online-Logo-2.png',
 
     DISABLE_DOMINANT_SPEAKER_INDICATOR: false,
 
